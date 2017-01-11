f8a3c27489cf7b6f585427e6c704cc5054a3114d
Tony Ngo <tony2ngo@gmail.com>
2017-01-10 16:55:45 -0800

    Test

--- js/UnityWebVRManager.js
+++ js/UnityWebVRManager.js
@@ -238,6 +238,9 @@
   }
 
   function getPose () {
+    if (!vrDisplay) {
+      return;
+    }
     if (isDeprecatedAPI) {
       return vrSensor.getState();
     } else {
@@ -277,11 +280,8 @@
 
   function getVRSensorState () {
     vrPose = getPose();
-    if (!vrPose) {
+    if (!vrPose || vrPose.orientation === null) {
       return;
-    }
-    if (vrPose.orientation === null) {
-        return;
     }
     var quaternion = isDeprecatedAPI ? vrPose.orientation : new THREE.Quaternion().fromArray(vrPose.orientation);
     var euler = new THREE.Euler().setFromQuaternion(quaternion);
